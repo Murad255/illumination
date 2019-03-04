433a379a7ba9a5448d082a72775866725f7ac701
Murad <murad.abdulzagirov@gmail.com>
2019-03-04 20:13:01 +0300

    #VERSION-1.1
    Были убраны некоторые переменные в файле [PWM.h] и оптимизированы функции, использовавшие их.

--- illumination/illumination/lib/PWM.h
+++ illumination/illumination/lib/PWM.h
@@ -12,13 +12,13 @@ typedef unsigned char byte;
 #ifndef PWM_H_
 #define PWM_H_
 #define DDR DDRB
+#define PORT PORTB
 #define PWM_PIN 0
-bool f=0;
-static byte i;   //последнее значение €ркости
+
 
 							
 void ledOff();				
-void ledOn(byte bright=i);	
+void ledOn(byte bright=0);	
 void ledInvert();			
 void PWMbegin();			
 void Up();					
@@ -26,35 +26,33 @@ void Up();
 
 //##############################################################
 void Up(){
-	if(i>4){
-	ledOff();
-	i=0;
-	}
-	else ledOn(++i);
+	if(OCR0A==0xFF)ledOff();
+	else ledOn(OCR0A/51+1);
 	return;
 }
-	void PWMbegin(){
-		DDR =1<<PWM_PIN;
-		TCCR0A=0b10100011; //выбираем неинверсный режим шим дл€ обоих светодиодов
-		TCCR0B=0b00000001;  //выбираем работу таймера без предделител€ тактовой частоты
+
+void PWMbegin(){
+	DDR |=1<<PWM_PIN;
+	MCUCR|=1<<PUD;
+	TCCR0A=0b10100011; //выбираем неинверсный режим шим дл€ обоих светодиодов
+	TCCR0B=0b00000001;  //выбираем работу таймера без предделител€ тактовой частоты
 	}
+	
 void ledOff(){
 	OCR0A=0;
-	f=0;
 	return;
 }
 
 void ledOn(byte bright){
-	f=1;
-	i=bright;
-	OCR0A=(bright*51);
+	 static byte n;
+		if (bright)OCR0A= n =(bright*51);
+		else  OCR0A=n;
 	return;
 }
 
 void ledInvert(){
-	
-	if(f==0)ledOn();
-	else if(f==1)ledOff();
+	if(OCR0A)ledOff();
+	else ledOn();
 	return;
 }
 #endif /* PWM_H_ */
